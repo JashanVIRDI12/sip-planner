3e9f3911405eb80fef7bfeb192b3fe23a614bfb9
JashanVIRDI12 <90898716+JashanVIRDI12@users.noreply.github.com>
2025-04-19 19:07:25 +0530

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Makes your Next.js site static (for Netlify, Surge, etc.)
+    output: 'export',
     images: {
-        unoptimized: true, // If you're using <Image />, this avoids SSR image optimization issues
+        unoptimized: true,
     },
 }
 
 export default nextConfig
+

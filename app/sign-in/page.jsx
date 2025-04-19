b56ba381a697448623fba3fcf4d214042ed09e4f
JashanVIRDI12 <90898716+JashanVIRDI12@users.noreply.github.com>
2025-04-19 18:47:22 +0530

    Update page.jsx

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -4,6 +4,8 @@ import { useRouter, useSearchParams } from 'next/navigation'
 import { supabase } from '@/lib/supabaseClient'
 import { Eye, EyeOff } from 'lucide-react'
 
+export const dynamic = 'force-dynamic'
+
 export default function AuthPage() {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
